49e626cc818764d008dc9910f118abbef2a1563d
charg2dang <chargedang@gmail.com>
2020-06-01 03:36:00 +0900

    leave 처리. 섹터에서 지우고. 상대방들의 시야에서 지윰.

--- 4차 과제/EchoServer/MMOServer.cpp
+++ 4차 과제/EchoServer/MMOServer.cpp
@@ -28,8 +28,33 @@ void MMOServer::on_create_sessions(size_t capacity)
 void MMOServer::on_connect(uint64_t session_id){}
 void MMOServer::on_disconnect(uint64_t session_id) 
 {
+	MMOActor*	my_actor = get_actor(session_id);
+	int			my_actor_id = my_actor->get_id();
+	MMOSector*	my_actor_sector = my_actor->current_sector;
+	AcquireSRWLockExclusive(&my_actor->lock);
 	
+	AcquireSRWLockExclusive(&my_actor_sector->lock);
+	my_actor_sector->actors.erase(my_actor_id);
+	ReleaseSRWLockExclusive(&my_actor_sector->lock);
 
+	auto& view_list = my_actor->view_list;
+	for( auto& actor_iter : view_list)
+	{
+		//if (actor_iter.second == my_actor) continue;
+		AcquireSRWLockExclusive(&actor_iter.second->lock); // 데드락 가능성 있다;
+		if (0 != actor_iter.second->view_list.count(my_actor_id))
+		{
+			ReleaseSRWLockExclusive(&actor_iter.second->lock);
+			actor_iter.second->send_leave_packet(my_actor);
+		}
+		else
+		{
+			ReleaseSRWLockExclusive(&actor_iter.second->lock);
+		}
+	}
+	view_list.clear();
+
+	ReleaseSRWLockExclusive(&my_actor->lock);
 }
 bool MMOServer::on_accept(Session* session) 
 { 
@@ -62,7 +87,7 @@ void MMOServer::on_start()
 	return;
 }
 
-MMOActor* MMOServer::getActor(uint64_t session_id)
+MMOActor* MMOServer::get_actor(uint64_t session_id)
 {
 	return ((MMOSession*)sessions[(uint16_t)session_id])->get_actor();
 }
